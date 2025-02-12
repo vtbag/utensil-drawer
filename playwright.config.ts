ecd7b743ee3e4802d65ed53cc49eb88744ef1932
Martin Trapp <94928215+martrapp@users.noreply.github.com>
2025-02-12 19:47:47 +0100

    be more patient

--- playwright.config.ts
+++ playwright.config.ts
@@ -3,6 +3,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
 	testDir: './tests',
 	workers:	1,
+	retries:	1,
 	timeout:	10000,
 	webServer: {
 		command: 'npm run start',
--- tests/declarative-names.spec.ts
+++ tests/declarative-names.spec.ts
@@ -9,7 +9,7 @@ test('call', async ({ page, browserName }) => {
 	page.on("console", msg => msg.text().startsWith("test ") && (text += msg.text().slice(4)));
 	await page.goto("http://localhost:3000/page2/");
 	await expect(page).toHaveTitle("Page 2");
-	await new Promise<void>(resolve => setTimeout(resolve, 600));
+	await new Promise<void>(resolve => setTimeout(resolve, 1000));
 	expect (text).toBe(" te\\~st1-0 test\\!2-0 true true true vtbag-decl-2-1");
 	text = "";
 });
