76206a136a7efe012ee919bc9eb3be4b03f46acd
Martin Trapp <94928215+martrapp@users.noreply.github.com>
2025-04-01 16:01:20 +0200

    on my way!

--- tests/declarative-names.spec.ts
+++ tests/declarative-names.spec.ts
@@ -1,7 +1,7 @@
-import { test, expect, Page } from '@playwright/test';
-test('call', async ({ page, browserName }) => {
+import { test, expect } from '@playwright/test';
+test('call', async ({ page }) => {
 	let text = "";
-	page.on("console", msg => (console.log("XXX " + msg.text()), msg.text().startsWith("test ") && (text += msg.text().slice(4))));
+	page.on("console", msg => (msg.text().startsWith("test ") && (text += msg.text().slice(4))));
 	await page.goto("http://localhost:3000/page2/");
 	await expect(page).toHaveTitle("Page 2");
 	await new Promise<void>(resolve => setTimeout(resolve, 1000));
