e89c11656a9f59097ede1179c86cd631e627203d
Martin Trapp <94928215+martrapp@users.noreply.github.com>
2025-04-01 12:32:40 +0200

    t&e

--- tests/drawer.spec.ts
+++ tests/drawer.spec.ts
@@ -3,7 +3,7 @@ import { test, expect } from '@playwright/test';
 test('call', async ({ page, browserName }) => {
 
 	let text = "";
-	page.on("console", (msg => msg.text().startsWith("test ") && (text += msg.text().slice(4))));
+	page.on("console", msg => (msg.text().startsWith("test ") && (text += msg.text().slice(4)) && console.log("XXX " + msg.text())));
 	await page.goto('http://localhost:3000/page1/');
 	await expect(page).toHaveTitle("Page 1");
 	await page.locator('button').click();
