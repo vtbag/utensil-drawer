9e7051bf5b36811d42ae04a28156d73fcad02a31
Martin Trapp <94928215+martrapp@users.noreply.github.com>
2025-04-02 23:54:05 +0200

    adds setVectors()

---
+++ .changeset/short-lions-smell.md
@@ -0,0 +1,5 @@
+---
+'@vtbag/utensil-drawer': patch
+---
+
+Adds experimental setVectors() function that makes parameters of morph animations accessible in CSS rules.
--- src/vectors.ts
+++ src/vectors.ts
@@ -1,34 +1,28 @@
-import { addStyle, initStyles } from './dynamic-style';
 
 export function setVectors() {
-	initStyles('vtbag-vectors');
-	document.getAnimations().forEach((animation) => {
+	const styles:string[]=[];
+	document.body.getBoundingClientRect();
+	document.getAnimations().forEach((animation: any) => {
+		if (!animation.animationName.startsWith("-ua-view-transition-group")) return;
 		const effect = animation.effect;
 		const pseudo = effect?.pseudoElement;
-		if (effect instanceof KeyframeEffect && pseudo?.startsWith('::view-transition-group')) {
-			const group = pseudo.slice(24, -1);
-			const frames = effect.getKeyframes();
-			let from = '' + frames[0]?.transform || 'none';
-			let to = '' + frames[frames.length - 1]?.transform || 'none';
-			if (to === 'none') {
-				const end = effect.getComputedTiming().endTime;
-				animation.currentTime = end || 0;
-				to =
-					getComputedStyle(effect?.target || document.documentElement, pseudo).transform || 'none';
-				animation.currentTime = 0;
-			}
-			if (from === 'none') {
-				from =
-					getComputedStyle(effect?.target || document.documentElement, pseudo).transform || 'none';
-			}
-			if (from === 'none' || to === 'none') return;
+		const group = pseudo.slice(24, -1);
+		const frames = effect.getKeyframes();
+		let from = '' + frames[0]?.transform || 'none';
+		let to = '' + frames[frames.length - 1]?.transform || 'none';
+		if (to === 'none') {
+			const end = effect.getComputedTiming().endTime;
+			animation.currentTime = end || 0;
+			to = getComputedStyle(effect?.target || document.documentElement, pseudo).transform || 'none';
+			animation.currentTime = 0;
+		}
+		if (from === 'none' || to === 'none') return;
 
-			const fromValues = new DOMMatrixReadOnly(from);
-			const toValues = new DOMMatrixReadOnly(to);
-
-			addStyle(
-				'vtbag-vectors',
-				`
+		const fromValues = new DOMMatrixReadOnly(from);
+		const toValues = new DOMMatrixReadOnly(to);
+		styles.push(
+			'vtbag-vectors',
+			`
         :root {
           --vtbag-vector-${group}-from-x: ${fromValues.e}px;
           --vtbag-vector-${group}-to-x: ${toValues.e}px;
@@ -41,7 +35,8 @@ export function setVectors() {
           --vtbag-vector-from-y: ${fromValues.f}px;
           --vtbag-vector-to-y: ${toValues.f}px;
         }`
-			);
-		}
+		);
+
 	});
+  document.head.insertAdjacentHTML('beforeend', `<style id="vtbag-dynamic-styles-vtbag-vectors">${styles.join('\n')}</style>`);
 }
